384cd951cf33f2d5b254de23142f869c64782be1
kiyah <z4kiy4h.h@gmail.com>
2026-01-15 09:24:44 +0700

    add particle background + install react + install ogl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
     // Add other configurations as needed, e.g., for Markdown
     markdown: {
         shikiConfig: {
